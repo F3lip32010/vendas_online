ee583e4f1e5b2cea0d47fe039c6694b5b934f649
Felipe <felipemeirelessantana@gmail.com>
2024-01-18 15:39:09 -0300

    c7 - loginScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
+import LoginScreen from './modules/login';
 import React from 'react';
-import NovoMenu from './DivMeu';
 
 function App() {
-  return (
-    <div>
-      <NovoMenu titulo="julio">
-        <NovoMenu titulo="jose" />
-        <NovoMenu titulo="maria" />
-        <NovoMenu titulo="jsgsgsgsegtsa" />
-      </NovoMenu>
-
-      <div>
-        <h1>fafdafadfdadf</h1>
-        <h2>jaçghsadgfh</h2>
-      </div>
-      <div>
-        <p>jfkljaç khçlkfçlghçlsakfdjçalk jçlakj çlkdjsaç lkjaçldfkj lçkjk</p>
-      </div>
-    </div>
-  );
+  return <LoginScreen />;
 }
 
 export default App;
